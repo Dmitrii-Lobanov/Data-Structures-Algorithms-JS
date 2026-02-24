b60525642daf23c498986d72bda79660289d8a4b
Dmitrii <dmitriilobanov3@gmail.com>
2026-02-24 08:25:33 -0500

    Data Structures - Arraya

---
+++ DataStructures/Arrays/reverseString.ts
@@ -0,0 +1,20 @@
+// Create a function that reverses a string
+
+function reverseString(str: string): string {
+    if (!str || str.length < 2 || typeof str !== "string") {
+        return "Please provide a string with more than 1 character";
+    }
+
+  // Step 1. Create an empty string that will host the new created string
+  let newString = "";
+
+  // Step 2. Create a for loop that will iterate through the string provided as an argument in reverse
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+
+  // Step 3. Return the reversed string
+  return newString;
+}
+
+console.log(reverseString("Create a function that reverses a string"));
